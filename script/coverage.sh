ab522c8a68e6ad3e1e3913bf77076ea20985d2f7
Nick <handnf@gmail.com>
2019-03-29 17:18:36 -0400

    Update coverage.sh
    Removes one test, fixes threshold and output

--- script/coverage.sh
+++ script/coverage.sh
@@ -10,26 +10,27 @@ if [ $? -eq 0 ]; then
   exit 0 # Exit early without error as we don't want to run coverage
 fi
 
-# Run coverage on most recent HEAD
 # Make sure we get the most recent commit hash
 curr_hash="$(git rev-parse HEAD)"
+# We already ran the test on the current version
+# File is stored in coverage/coverage-summary.json
+cvg_pct="console.log(require('./coverage/coverage-summary.json').total.functions.pct)"
+cvg_pct_threshold="console.log(require('./coverage/coverage-summary.json').total.functions.pct + 3)"
+curr_coverage=$(node -e "$cvg_pct")
+curr_coverage_threshold=$(node -e "$cvg_pct_threshold")
+# Go back a step in the git chain
 git checkout HEAD~1
 # We are in HEAD-1
 # Run coverage check and get it output as ./coverage/coverage-summary.json
 yarn test > /dev/null
 # File is stored in coverage/coverage-summary.json
-coverage_pct="console.log(require('./coverage/coverage-summary.json').total.functions.pct)"
-prev_coverage=$(node -e $coverage_pct)
+prev_coverage=$(node -e $cvg_pct)
 
-# Now go to this commit
+# Now return to the commit
 git checkout "$curr_hash"
 # Run coverage check and get it output as ./coverage/coverage-summary.json
-yarn test > /dev/null
-# File is stored in coverage/coverage-summary.json
-coverage_pct_threshold="console.log(require('./coverage/coverage-summary.json').total.functions.pct + 3)"
-curr_coverage=$(node -e "$coverage_pct_threshold")
 
-if [ $prev_coverage -lt $curr_coverage ]; then
+if [ $prev_coverage -lt $curr_coverage_threshold ]; then
   # This change reduces function code coverage.
   # This is not good.
   echo "This change reduces code coverage from ${prev_coverage}% to ${curr_coverage}%"
